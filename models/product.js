dff624af61e747f16bef8952f4d3bcda56cbcf83
Shawn Smith <srsmith11@live.com>
2021-03-13 13:26:33 -0700

    added delete product to admin and cart

--- models/product.js
+++ models/product.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const Cart = require('./cart')
+
 const p = path.join(
   path.dirname(process.mainModule.filename),
   'data',
@@ -47,12 +49,13 @@ module.exports = class Product {
 
   static deleteById(id) {
     getProductsFromFile(products => {
-      const pupdatedProducts = products.filter(p => p.id !== id);
-      fs.writeFile([, JSON.stringify(updatedProducts), err => {
+      const product = products.find(prod => prod.id === id);
+      const updatedProducts = products.filter(p => p.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
         if (!err) {
-          //delete from cart
+          Cart.deleteProduct(id, product.price);
         }
-      }])
+      })
     });
   }
 
