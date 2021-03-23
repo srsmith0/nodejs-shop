6591fe32158e77d54d85f163d37545c7a3512784
Shawn Smith <srsmith11@live.com>
2021-03-22 20:42:42 -0600

    cart and order CRUD complete

--- models/users.js
+++ models/users.js
@@ -76,9 +76,19 @@ class User {
 
   addOrder() {
     const db = getDb();
-    return db
-      .collection('orders')
-      .insertOne(this.cart)
+    return this.getCart()
+    .then(products => {
+      const order = {
+        items: products,
+        user: {
+          _id: new ObjectId(this._id),
+          name: this.name
+        }
+      };
+      return db
+        .collection('orders')
+        .insertOne(order)
+     })
       .then(result => {
         this.cart = {items: []};
     return db
@@ -87,8 +97,15 @@ class User {
        { _id: new ObjectId(this._id)}, 
         { $set: {cart: {items: []}} }
       );
-
     })
+  }
+
+  getOrders() {
+    const db = getDb();
+    return db
+    .collection('orders')
+    .find({'user._id': new ObjectId(this._id)})
+    .toArray();
   }
 
   static findById(userId) {
