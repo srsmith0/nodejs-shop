30d5814ef5eb3a125000f56215af40b4724b6e61
Shawn Smith <srsmith11@live.com>
2021-03-29 20:24:11 -0600

    added order crud actions

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-// const User = require('../models/users');
+const Order = require('../models/order');   
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -75,28 +75,44 @@ exports.postCartDeleteProduct = (req, res, next) => {
     res.redirect('/cart');
   })
   .catch(err => console.log(err));
-}
+};
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
-  req.user
-    .addOrder()
+    req.user
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items.map(i => {
+        return {quantity: i.quantity, productData: { ...i.productId._doc } };
+      });
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user
+        },
+        products: products,
+      });
+      return order.save();
+    })
     .then(result => {
+      return req.user.clearCart();
+    }).then(() => {
       res.redirect('/orders');
     })
     .catch(err => console.log(err));
-}
+};
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
-    .then(orders => {
-      res.render('shop/orders', {
-        path: '/orders',
-        pageTitle: 'Your Orders',
-        orders: orders
-      });
-    })
+  Order.find({
+    "user.userId": req.user._id
+  })
+  .then(orders => {
+    res.render('shop/orders', {
+      path: '/orders',
+      pageTitle: 'Your Orders',
+      orders: orders
+    });
+  })
     .catch(err => console.log(err));
 };
 
