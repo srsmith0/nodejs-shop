0f3baba62ea9092854110b557fcab5b0ab10a31a
Shawn Smith <srsmith11@live.com>
2021-04-14 17:48:29 -0600

    added validation to post edit

--- controllers/admin.js
+++ controllers/admin.js
@@ -8,7 +8,8 @@ exports.getAddProduct = (req, res, next) => {
     path: '/admin/add-product',
     editing: false,
     hasError: false,
-    errorMessage: null
+    errorMessage: null,
+    validationErrors: []
   });
 };
 
@@ -31,9 +32,11 @@ exports.postAddProduct = (req, res, next) => {
           price,
           description
         },
-        errorMessage: errors.array()[0].msg
+        errorMessage: errors.array()[0].msg,
+        validationErrors: errors.array()
       });
   }
+
   const product = new Product({
     title: title,
     price: price,
@@ -70,7 +73,8 @@ exports.getEditProduct = (req, res, next) => {
         editing: editMode,
         product: product,
         hasError: false,
-        errorMessage: null
+        errorMessage: null,
+        validationErrors: []
       });
     })
     .catch(err => console.log(err));
@@ -82,6 +86,25 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        editing: true,
+        hasError: true,
+        product: {
+          _id: prodId,
+          title: updatedTitle,
+          imageUrl: updatedImageUrl,
+          price: updatedPrice,
+          description: updatedDesc
+        },
+        errorMessage: errors.array()[0].msg,
+        validationErrors: errors.array()
+      });
+  }
 
   Product.findById(prodId)
     .then(product => {
@@ -102,8 +125,6 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.find({ userId: req.user._id })
-    // .select('title price -_id')
-    // .populate('userId', 'name')
     .then(products => {
       console.log(products);
       res.render('admin/products', {
