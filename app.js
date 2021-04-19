1a65a573cc1c66aa56c0aa204c441040bbcc1ba1
Shawn Smith <srsmith11@live.com>
2021-04-18 21:42:56 -0600

    configured file upload w/multer

--- app.js
+++ app.js
@@ -21,6 +21,23 @@ const store = new MongoDBStore({
 });
 const csrfProtection = csrf();
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+    cb(null, true);
+  } else {
+  cb(null, false);
+  };
+};
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -30,7 +47,7 @@ const authRoutes = require('./routes/auth');
 
 // app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.urlencoded());
-app.use(multer({dest: 'images'}).single('image'));
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
